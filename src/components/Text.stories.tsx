dede73b24f16b5a756359e05851f8f04e00acb1d
Carlos <carlos.msv@hotmail.com>
2022-11-23 20:47:39 -0300

    text component interface + clsx

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,12 @@
-import { Text } from './Text'
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from './Text'
 
 export default {
   title: 'Components/Text',
   component: Text,
-}
+  args: {
+    children: 'Lorem ipsum.'
+  },
+} as Meta<TextProps>
 
-export const Default = {}
+export const Default: StoryObj = {}
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,6 +1,22 @@
+import { clsx } from 'clsx'; 
 
-export function Text(){
+export interface TextProps {
+  size?: 'sm' | 'md' | 'lg';
+  children: string; 
+}
+
+export function Text({ size = 'md', children }: TextProps){
   return (
-    <span className='text-gray-100 font-sans'>Hello World</span>
+    <span className={clsx(
+      'text-gray-100 font-sans', 
+      {
+        'text-xs': size == 'sm',
+        'text-sm': size == 'md',
+        'text-md': size == 'lg',
+      }
+    )}
+    >
+      {children}
+    </span>
   )
 }
