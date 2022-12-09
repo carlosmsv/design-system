0dca2ef598b53bf0119ec37a364fceb44ccb7e7d
Carlos <carlos.msv@hotmail.com>
2022-12-08 23:14:58 -0300

    footer text hover

--- src/App.tsx
+++ src/App.tsx
@@ -57,10 +57,10 @@ export function App() {
 
         <footer className="flex flex-col items-center gap-4 mt-8">
           <Text asChild size="sm">
-            <a href="" className="text-gray-400 underline">Esqueceu sua senha?</a>
+            <a href="" className="text-gray-400 underline hover:text-gray-200">Esqueceu sua senha?</a>
           </Text>
           <Text asChild size="sm">
-            <a href="" className="text-gray-400 underline">Não possui conta? Cire uma agora!</a>
+            <a href="" className="text-gray-400 underline hover:text-gray-200">Não possui conta? Cire uma agora!</a>
           </Text>
         </footer>
       </div>
