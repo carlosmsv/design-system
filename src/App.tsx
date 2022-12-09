375c7e0b848754530618c70cdec3c635ee3ccabe
Carlos <carlos.msv@hotmail.com>
2022-12-08 22:42:26 -0300

    refactoring App.tsx, using Logo (from Figma) to component (via transform.tools)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { Logo } from './Logo'
 import './styles/global.css'
 
 export function App() {
 
   return (
     <>
-      <h1 className='font-bold text-2xl text-violet-500'>Hello World</h1>
-
-      <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300"> Enviar </button>
+      <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100">
+        <Logo/>
+      </div>
     </>
   )
 }
