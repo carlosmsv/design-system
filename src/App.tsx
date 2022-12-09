534ad87daf8ee233b3edc81f8cc9d923ac190f53
Carlos <carlos.msv@hotmail.com>
2022-12-08 22:58:13 -0300

    password input

--- src/App.tsx
+++ src/App.tsx
@@ -22,14 +22,24 @@ export function App() {
           </Text>
         </header>
 
-        <form className="flex flex-col items-stretch">
+        <form className="flex flex-col gap-4 items-stretch w-full max-w-sm mt-10">
           <label htmlFor="email" className="flex flex-col gap-3">
             <Text className="font-semibold">Endereço de e-mail</Text>
             <TextInput.Root>
               <TextInput.Icon>
                 <Envelope />
               </TextInput.Icon>
-              <TextInput.Input placeholder="Digite seu e-mail"/>
+              <TextInput.Input type="email" id="email" placeholder="Digite seu e-mail"/>
+            </TextInput.Root>
+          </label>
+
+          <label htmlFor="password" className="flex flex-col gap-3">
+            <Text className="font-semibold">Sua senha</Text>
+            <TextInput.Root>
+              <TextInput.Icon>
+                <Envelope />
+              </TextInput.Icon>
+              <TextInput.Input type="password"id="password" placeholder="********"/>
             </TextInput.Root>
           </label>
         </form>
