c78d4b639071bccc63bf7580f39d2484a3ea0d79
Carlos <carlos.msv@hotmail.com>
2022-12-08 22:55:34 -0300

    added header and form

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { Envelope } from 'phosphor-react'
+import { Heading } from './components/Heading'
+import { Text } from './components/Text'
+import { TextInput } from './components/TextInput'
 import { Logo } from './Logo'
 import './styles/global.css'
 
@@ -5,8 +9,30 @@ export function App() {
 
   return (
     <>
-      <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100">
-        <Logo/>
+      <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
+        <header className="flex flex-col items-center">
+          <Logo/>
+          
+          <Heading size="lg" className="mt-4">
+            Ignite Lab
+          </Heading>
+
+          <Text size="lg" className="text-gray-400 mt-1">
+            Faça login e comece a usar
+          </Text>
+        </header>
+
+        <form className="flex flex-col items-stretch">
+          <label htmlFor="email" className="flex flex-col gap-3">
+            <Text className="font-semibold">Endereço de e-mail</Text>
+            <TextInput.Root>
+              <TextInput.Icon>
+                <Envelope />
+              </TextInput.Icon>
+              <TextInput.Input placeholder="Digite seu e-mail"/>
+            </TextInput.Root>
+          </label>
+        </form>
       </div>
     </>
   )
